5316691273008f5c20e86bce54446f427906afb0
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-07-28 15:34:09 -0300

    first commit

---
+++ src/main/java/org/codechill/tartaruscms/TartarusCmsApplication.java
@@ -0,0 +1,13 @@
+package org.codechill.tartaruscms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TartarusCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TartarusCmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/codechill/tartaruscms/TartarusCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codechill.tartaruscms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TartarusCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
