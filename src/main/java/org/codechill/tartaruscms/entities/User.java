68071023816b4a52f5c9ef39d9443685d7e6c01d
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-07-29 18:10:36 -0300

    added `@NoArgsConstructor`

--- src/main/java/org/codechill/tartaruscms/entities/User.java
+++ src/main/java/org/codechill/tartaruscms/entities/User.java
@@ -4,11 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class User implements Serializable {
     @Id
     Long id;
