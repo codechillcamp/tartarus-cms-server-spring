9842c0903c4838222871e37ad948e0706382fd02
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-07-30 19:56:57 -0300

    ProductController Modified
    Endpoints modified and params on methods

--- src/main/java/org/codechill/tartaruscms/controllers/ProductController.java
+++ src/main/java/org/codechill/tartaruscms/controllers/ProductController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
 public class ProductController {
     private final IProductService productService;
 
@@ -19,27 +18,27 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("")
+    @GetMapping("/api/v1/products")
     public List<Product> findAll() {
         return productService.findAll();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/api/v1/products/{id}")
     public Product findById(@PathVariable Long id) {
         return productService.findById(id);
     }
 
-    @PostMapping("/{storeId}")
-    public Product create(@RequestBody CreateProductRequest product) {
-        return productService.create(product);
+    @PostMapping("/api/v1/products/store/{storeId}")
+    public Product create(@RequestBody CreateProductRequest product, @PathVariable Long storeId) {
+        return productService.create(product, storeId);
     }
 
-    @PatchMapping("/{id}")
-    public Product update(@RequestBody UpdateProductRequest product) {
-        return productService.update(product);
+    @PatchMapping("/api/v1/products/{id}")
+    public Product update(@PathVariable Long id, @RequestBody UpdateProductRequest product) {
+        return productService.update(product, id);
     }
 
-    @GetMapping("/all/{storeId}")
+    @GetMapping("/api/v1/products/all/{storeId}")
     public List<Product> findAllByStoreId(@PathVariable Long storeId) {
         return productService.findAllByStoreId(storeId);
     }
