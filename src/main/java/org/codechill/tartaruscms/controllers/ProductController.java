a3641ff2b6e91277947e4c913c5a1f3924b49f29
Rodrigo Caraballo <rodrigomcaraballo8@gmail.com>
2023-07-30 17:28:16 -0300

    ProductController Modified
    DTOs added, Routers modified and new method

--- src/main/java/org/codechill/tartaruscms/controllers/ProductController.java
+++ src/main/java/org/codechill/tartaruscms/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package org.codechill.tartaruscms.controllers;
 
+import org.codechill.tartaruscms.CreateProductRequest;
+import org.codechill.tartaruscms.dto.UpdateProductRequest;
 import org.codechill.tartaruscms.entities.Product;
 import org.codechill.tartaruscms.services.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,12 +30,17 @@ public class ProductController {
     }
 
     @PostMapping("/{storeId}")
-    public Product create(@RequestBody Product product, @PathVariable Long storeId) {
+    public Product create(@RequestBody CreateProductRequest product, @PathVariable Long storeId) {
         return productService.create(product, storeId);
     }
 
     @PatchMapping("/{id}")
-    public Product update(@PathVariable Long id, @RequestBody Product product) {
+    public Product update(@PathVariable Long id, @RequestBody UpdateProductRequest product) {
         return productService.update(product, id);
+    }
+
+    @GetMapping("/all/{storeId}")
+    public List<Product> findAllByStoreId(@PathVariable Long storeId) {
+        return productService.findAllByStoreId(storeId);
     }
 }
